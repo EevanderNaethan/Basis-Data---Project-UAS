4f028a8f59cd655865cf1a2ae1138dd99bd8f2b3
EevanderNaethan <ivandernathan22@gmail.com>
2024-12-18 17:33:43 +0700

    Ivander done

--- BasisData_ProjectUAS/blog.php
+++ BasisData_ProjectUAS/blog.php
@@ -18,14 +18,119 @@
   <?php include "style.css" ?>
 </style>
 
+
   </head>
   <body>
     <!-- BUNG IVANDER -->
 
 
 
-    <h1>Hello, world!</h1>
+    <div class="container-fluid">
+  <div class="navbar">
+    <h1>PustakaKita</h1>
 
+    <ul>
+    <li><a href="#">
+      Beranda
+    </a></li>
+    <li><a href="#">
+      Tentang Kami
+    </a></li>
+    <li><a href="#">
+      Koleksi Buku
+    </a></li>
+    <li><a href="#">
+      Blog
+    </a></li>
+    </ul>
+    
+    <div class="icon_nav">
+      <a href="#"><i class="bi bi-book"></i></a>
+      <a href="#" class="btn_icon">Ayo Baca</a>
+    </div>
+  
+    
+  </div>
+
+<div class="blog_bar">
+  <h1>blog</h1>
+</div>
+
+<div class="blog-list row">
+  <!-- page 1 -->
+  <div class="col-md-4">
+    <img src="asset\keluarga_saling_membaca.png" alt="">
+    <div class="blog-para">
+    <h2>Untuk anak dan orang tua: buku untuk dibaca bersama</h2>
+    <p>Membaca bersama mempererat hubungan anak dan orang tua. Temukan buku-buku seru dan edukatif yang bisa dinikmati bersama, sambil menumbuhkan kecintaan membaca sejak dini.</p>
+    <a href="#">Baca seluruh post</a>
+    </div>
+    
+  </div>
+  <!-- page 2 -->
+  <div class="col-md-4">
+    <img src="asset\buku-romansa.png" alt="">
+    <div class="blog-para">
+    <h2>Kisah cinta untuk setiap selera: lucu, aneh, dalam, tragis</h2>
+    <p>Beragam kisah cinta untuk dinikmati — dari yang lucu, aneh, mendalam, hingga tragis. Temukan cerita yang sesuai dengan seleramu!</p>
+    <a href="#">Baca seluruh post</a>
+    </div>
+
+    <!-- page 3 -->
+
+  </div>
+  <div class="col-md-4">
+    <img src="asset\Embak-membaca.png" alt="">
+    <div class="blog-para">
+    <h2>Bukan hanya bakat: buku untuk membantu Anda percaya diri dan berhasil</h2>
+    <p>Membaca bersama mempererat hubungan anak dan orang tua. Temukan buku-buku seru dan edukatif yang bisa dinikmati bersama, sambil menumbuhkan kecintaan membaca sejak dini.</p>
+    <a href="#">Baca seluruh post</a>
+    </div>
+    
+  </div>
+
+  
+</div>
+<!-- page 4 -->
+<div class="blog-list row">
+  <div class="col-md-4">
+    <img src="asset\Adryan-nungging-hot.png" alt="">
+    <div class="blog-para">
+    <h2>Kolaborasi Mahasiswa: Strategi untuk Sukses Akademik</h2>
+    <p>Merekomendasi buku yang dapat membantu mahasiswa mengembangkan keterampilan kolaborasi dalam perkuliahan. Dari teknik komunikasi efektif hingga manajemen tim</p>
+    <a href="#">Baca seluruh post</a>
+    </div>
+    
+  </div>
+
+  <!-- page 5 -->
+
+  <div class="col-md-4">
+    <img src="asset\Laki-laki-membaca.png" alt="">
+    <div class="blog-para">
+    <h2>Buku sunyi: Apa Itu dan Bagaimana Cara Membacanya</h2>
+    <p>temukan cara membaca buku jenis ini dengan lebih mendalam dan menikmati pengalaman literasi yang lebih introspekti</p>
+    <a href="#">Baca seluruh post</a>
+    </div>
+    
+  </div>
+
+  <!-- page 6 -->
+
+  <div class="col-md-4">
+    <img src="asset\Link.png" alt="">
+    <div class="blog-para">
+    <h2>Prestasi Gemilang Alumni Universitas Indonesia yang Menginspirasi</h2>
+    <p>Alumni Universitas Indonesia telah meraih prestasi membanggakan di berbagai bidang. Simak kisah-kisah inspiratif mereka yang membawa perubahan positif dan menginspirasi banyak orang.</p>
+    <a href="#">Baca seluruh post</a>
+    </div>
+    
+  </div>
+
+  
+</div>
+
+</div>
 
 
 
